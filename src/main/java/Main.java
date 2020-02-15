58cf336ebc6df1d61b57972d22638c83d82bbbb1
Giles Bradford <gbradford@zipcodes-MacBook-Pro-4.local>
2020-02-15 13:51:52 -0500

    uploading with one test case failing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,6 @@ public class Main {
             for (int q = i; q < exampleArray.length; q++) {
                 for (int s = i; s <= q; s++) {
                     sum += exampleArray[s];
-                    System.out.println(exampleArray[s]);
                 }
             }
         }
@@ -17,7 +16,30 @@ public class Main {
     public Integer fasterSum(Integer[] exampleArray) {
         Integer sum = 0;
         for (int i = 0; i < exampleArray.length; i++) {
-            sum +=
+            sum += exampleArray[i] * (exampleArray.length - i) * (i + 1);
+        }
+        return sum;
+    }
+
+
+    public Long getSumLong(Integer[] exampleArray) {
+
+        Long sum = 0l;
+
+        for (int i = 0; i < exampleArray.length; i++) {
+            for (int q = i; q < exampleArray.length; q++) {
+                for (int s = i; s <= q; s++) {
+                    sum += exampleArray[s];
+                }
+            }
+        }
+        return sum;
+    }
+
+    public Long fasterSumLong(Integer[] exampleArray) {
+        Long sum = 0l;
+        for (int i = 0; i < exampleArray.length; i++) {
+            sum += exampleArray[i] * (exampleArray.length - i) * (i + 1);
         }
         return sum;
     }
